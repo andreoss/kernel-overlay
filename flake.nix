3438b164851b8b2027b28c98e0dfc2bef73fe1f8
andreoss <andreoss@sdf.org>
2023-07-28 23:21:58 -0400

    remove virtualbox

--- flake.nix
+++ flake.nix
@@ -49,13 +49,7 @@
                 system.stateVersion = "23.05";
                 boot.extraModulePackages = with config.boot.kernelPackages; [ ];
                 boot.kernelPackages = kpkgs.${mkName "linuxPackages" p};
-                virtualisation = {
-                  virtualbox.guest = { enable = true; };
-                  virtualbox.host = {
-                    enable = true;
-                    enableExtensionPack = true;
-                  };
-                };
+                virtualisation = {};
               })
             ];
           };
