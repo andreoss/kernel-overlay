5b8f8bf21d38557611c7b16b3f9866b834f61f65
andreoss <andreoss@sdf.org>
2024-04-13 21:53:16 -0400

    Update workflow

---
+++ update-workflow.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i perl -p perl538 -p perl538Packages.JSON -p perl538Packages.FileSlurp -p perl538Packages.DataDump -p perl538Packages.ModernPerl -p perl538Packages.TextSimpleTable
+
+use Text::SimpleTable;
+use Modern::Perl;
+use feature qw(say postderef);
+use File::Slurp;
+use JSON;
+
+my $json = JSON->new->utf8->pretty(1)->canonical(1)->allow_nonref;
+
+my $file = '.github/workflows/main.yml';
+
+my $x        = read_file('sources.json');
+my $sources  = $json->decode($x);
+my $f        = read_file($file);
+my $workflow = $json->decode($f);
+
+$workflow->{jobs}->{build}->{strategy}->{matrix}->{version}->@* =
+  sort { $b cmp $a } map { $_->{package}->{name} } $sources->@*;
+
+write_file( $file, $json->encode($workflow) );
+
+sub update_readme() {
+    my $f = 'README.md';
+    my $c = read_file($f);
+    my @d =
+      map { [ $_->{version} => $_ ] }
+      sort { $b->{version} cmp $c->{version} } $sources->@*;
+    my $m = "|Version|Package|Date|\n";
+    $m.= "|---|---|---|\n";
+    for my $k ( @d ) {
+        $m.="|";
+        $m.=  join "|", ($k->[0], "<b>" . $k->[1]->{package}->{name} . "</b>",
+            $k->[1]->{date} );
+        $m.="|\n";
+    }
+    $c =~ s{
+     (?<=\<\!--START--\>\R)
+      .*
+     (?=\<\!--END--\>\R)
+    }{$m}xgms;
+
+    write_file( $f, $c );
+}
+
+update_readme();
